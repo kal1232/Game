30f2ea39ceac0055a28f93493251d9cdcba407b3
ahreum_kim <ahreum.kim.95@gmail.com>
2019-04-01 20:50:29 +0900

    가위, 바위, 보 랜덤메소드 - getRandomWord()

--- game/src/game/game.java
+++ game/src/game/game.java
@@ -1,5 +1,26 @@
 package game;
 
-public class game {
+import java.util.Random;
+import java.util.Scanner;
 
+public class game{
+	 
+	static String rock = "바위";
+	static String paper = "보";
+	static String scissors = "가위";
+	static String com = "";
+ 
+	//가위,바위,보(랜덤으로 나오기)
+	public static void getRandomWord(){
+ 
+		String[] RandomPlay = {rock, paper, scissors};
+		Random random = new Random();
+		int ranNum = random.nextInt(RandomPlay.length);
+ 
+		com = RandomPlay[ranNum];
+ 
+		System.out.println(com);
+ 
+	}	
+	
 }
